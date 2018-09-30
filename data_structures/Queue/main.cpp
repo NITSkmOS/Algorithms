8aec698d4e70dd4ac202a6ca6635296c2d016f10
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2018-09-30 20:46:54 +0200

    Remove asserts and create a proof of concept

--- data_structures/Queue/main.cpp
+++ data_structures/Queue/main.cpp
@@ -4,14 +4,19 @@
 
 int main() {
     Queue<int> q;
+
+    // Add 1, 2, 3, 4 to a queue
+    q.push(1);
     q.push(2);
     q.push(3);
-    assert(!q.empty());
-    assert(q.top().value_or(-1) == 2);
-    q.pop();
-    assert(q.top().value_or(-1) == 3);
-    q.pop();
-    assert(q.empty());
-    assert(q.top().value_or(-1) == -1);
+    q.push(4);
+    // Print 1, 2, 3, 4
+    while (!q.empty()) {
+        std::cout << q.top().value_or(-1) << " ";
+        q.pop();
+    }
+
+    // Print -1 because the queue is empty
+    std::cout << q.top().value_or(-1) << "\n";
     return 0;
 }
