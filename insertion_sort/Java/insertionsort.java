d274d1d449f061eed2d15c321b142b9285c93629
Calvin <31526471+Om-Pandey@users.noreply.github.com>
2018-10-01 01:07:48 +0530

    insertionsort.java : Added InsertionSort Algorithm [Java]

--- insertion_sort/Java/insertionsort.java
+++ insertion_sort/Java/insertionsort.java
@@ -1,8 +1,29 @@
 package java;
 import java.util.*;
-class InsertionSort
-{
-    public static void main(String args[])
+class InsertionSort 
+{ 
+    /*Function to sort array using insertion sort*/
+    void insertionsort(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i=1; i<n; ++i) 
+        { 
+            int key = arr[i]; 
+            int j = i-1; 
+  
+            /* Move elements of arr[0..i-1], that are 
+               greater than key, to one position ahead 
+               of their current position */
+            while (j>=0 && arr[j] > key) 
+            { 
+                arr[j+1] = arr[j]; 
+                j = j-1; 
+            } 
+            arr[j+1] = key; 
+        } 
+    } 
+    // Driver method 
+     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of values :");
@@ -11,27 +32,12 @@ class InsertionSort
         int arr[] = new int[var];
         for(int i =0 ; i < var ;i++)
         arr[i] = sc.nextInt();
-
-        int n = arr.length;
-        for (int i=1; i<n; ++i)
-        {
-            int key = arr[i];
-            int j = i-1;
-
-            /* Move elements of arr[0..i-1], that are
-               greater than key, to one position ahead
-               of their current position */
-            while (j>=0 && arr[j] > key)
-            {
-                arr[j+1] = arr[j];
-                j = j-1;
-            }
-            arr[j+1] = key;
-        }
-          sc.close();
-
+        InsertionSort ob = new InsertionSort();
+        ob.insertionsort(arr);
+        sc.close();
+         int n = arr.length; 
           for (int i=0; i<n; ++i)
               System.out.print(arr[i] + " ");
           System.out.println();
     }
-}
+} 
