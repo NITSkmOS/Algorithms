787ead097d7a6d0c92bb989f09917ff7247f914e
shaurya uppal <shauryauppal00111@gmail.com>
2018-10-01 03:25:40 +0530

    Added LinkList C++

--- linked_list/C++/linklist_c++.cpp
+++ linked_list/C++/linklist_c++.cpp
@@ -5,27 +5,31 @@ struct node
 {
     int data;
     node *next;
-}*ptr;
-node *insert(node * top,int n)
+};
+//Inserting of data in LinkList
+node *insert(node *top,int val)
 {
-    ptr=new node;
-    ptr->data=n;
+    node *ptr = new node;
+    ptr->data=val;
     ptr->next=NULL;
 
+    //If LinkList is empty
     if(top==NULL)
     {
         top=ptr;
     }
     else{
+            //Insertion in Beginning of LinkList
             ptr->next=top;
-              top=ptr;
+            top=ptr;
     }
     return top;
 }
+//Print LinkList
 void display(node *top)
 {
-    ptr=top;
-    while(ptr!=NULL)
+    node *ptr=top;
+    while(ptr)
     {
         cout<<ptr->data<<" ";
         ptr=ptr->next;
@@ -34,9 +38,13 @@ void display(node *top)
 int main()
 {
     node *top=NULL;
+    //LL : 23->NULL
     top=insert(top,23);
+    //LL: 3243->23->NULL
     top=insert(top,3243);
+    //LL:213->3243->23->NULL
     top=insert(top,213);
+    //Print the LinkList
     display(top);
     return 0;
 }
