45a2c148db675282cf0de4c6262ee2fea2d942e5
srinidhi <kss682@gmail.com>
2018-10-01 01:08:49 +0530

    Fixes #44

--- binary_search.c
+++ binary_search.c
@@ -1,16 +1,10 @@
 #include<stdio.h>
 #define MAX 100
-void main()
+
+
+void binarysearch(int array[],int x,int n)
 {
-int array[MAX],l,r,mid,n,x,flag=0,i;
-printf("enter the no of elements\n");
-scanf("%d", &n);
-printf("enter the elements of the sorted array\n");
-for(i=0;i<n;i++)
-{scanf("%d",&array[i]);
-}
-printf("enter the element to be searched\n");
-scanf("%d", &x);
+int l,r,mid,flag=0;
 l=0;
 r=n-1;
 mid=(l+r)/2;
@@ -32,5 +26,19 @@ else if(x>array[mid])
 }
 if(flag==0)
 	printf("The element is not found\n");
+
+}
+void main()
+{
+int array[MAX],x,n;
+printf("enter the no of elements\n");
+scanf("%d", &n);
+printf("enter the elements of the sorted array\n");
+for(int i=0;i<n;i++)
+{scanf("%d",&array[i]);
+}
+printf("enter the element to be searched\n");
+scanf("%d", &x);
+binarysearch(array,x,n);
 
 }
