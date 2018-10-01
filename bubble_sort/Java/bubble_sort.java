200b61a08a4272187033e2fe82d0dfca2558386c
Prabhat Kumar Sahu <prabhat1015@gmail.com>
2018-10-01 07:26:15 +0530

    Update bubble_sort.java

--- bubble_sort/Java/bubble_sort.java
+++ bubble_sort/Java/bubble_sort.java
@@ -1,33 +1,39 @@
-class BubbleSort
-{
-   void bubbleSort(int arr[])
-   {
-       int n = arr.length;
-       for (int i = 0; i < n-1; i++)
-           for (int j = 0; j < n-i-1; j++)
-               if (arr[j] > arr[j+1])
-               {
-                   // swap temp and arr[i]
-                   int temp = arr[j];
-                   arr[j] = arr[j+1];
-                   arr[j+1] = temp;
-               }
-   }
-   /* Prints the array */
-   void printArray(int arr[])
-   {
-       int n = arr.length;
-       for (int i=0; i<n; ++i)
-           System.out.print(arr[i] + " ");
-       System.out.println();
-   }
-   // Driver method to test above
-   public static void main(String args[])
-   {
-       BubbleSort ob = new BubbleSort();
-       int arr[] = {64, 34, 25, 12, 22, 11, 90};
-       ob.bubbleSort(arr);
-       System.out.println("Sorted array");
-       ob.printArray(arr);
-   }
+public class MyBubbleSort {
+  
+    // logic to sort the elements
+    public static void bubble_srt(int array[]) {
+        int n = array.length;
+        int k;
+        for (int m = n; m >= 0; m--) {
+            for (int i = 0; i < n - 1; i++) {
+                k = i + 1;
+                if (array[i] > array[k]) {
+                    swapNumbers(i, k, array);
+                }
+            }
+            printNumbers(array);
+        }
+    }
+  
+    private static void swapNumbers(int i, int j, int[] array) {
+  
+        int temp;
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+  
+    private static void printNumbers(int[] input) {
+          
+        for (int i = 0; i < input.length; i++) {
+            System.out.print(input[i] + ", ");
+        }
+        System.out.println("\n");
+    }
+  
+    public static void main(String[] args) {
+        int[] input = { 4, 2, 9, 6, 23, 12, 34, 0, 1 };
+        bubble_srt(input);
+  
+    }
 }
