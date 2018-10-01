ffc3b1744c3dfec8580c6d42c30ed18e12e8fd7a
parammittal16 <parammittal16@gmail.com>
2018-10-02 00:38:30 +0530

    Update avl_tree_cpp

--- AVL_Tree/Cpp/avl_tree.cpp
+++ AVL_Tree/Cpp/avl_tree.cpp
@@ -14,19 +14,19 @@ struct avl_node {
  * Class Declaration
  */
 class avlTree {
-  public:
-    int height(avl_node * );
-  int diff(avl_node * );
-  avl_node * rr_rotation(avl_node * );
-  avl_node * ll_rotation(avl_node * );
-  avl_node * lr_rotation(avl_node * );
-  avl_node * rl_rotation(avl_node * );
-  avl_node * balance(avl_node * );
+   public:
+    int height(avl_node *};
+  int diff(avl_node *};
+  avl_node * rr_rotation(avl_node *};
+  avl_node * ll_rotation(avl_node *};
+  avl_node * lr_rotation(avl_node *};
+  avl_node * rl_rotation(avl_node *};
+  avl_node * balance(avl_node *};
   avl_node * insert(avl_node * , int);
   void display(avl_node * , int);
-  void inorder(avl_node * );
-  void preorder(avl_node * );
-  void postorder(avl_node * );
+  void inorder(avl_node *};
+  void preorder(avl_node *};
+  void postorder(avl_node *};
   avlTree() {
     root = NULL;
   }
@@ -229,7 +229,6 @@ void avlTree::preorder(avl_node * tree) {
   cout << tree - > data << "  ";
   preorder(tree - > left);
   preorder(tree - > right);
-
 }
 
 /*
