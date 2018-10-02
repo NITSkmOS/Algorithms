4d73699e84a6ef55fd0e45204b5dd98179c1e549
akrishnakumar-splunk <akrishnakumar@splunk.com>
2018-10-02 16:47:16 +0530

    Updating the code for resolving the errors

--- queue/python/queue.py
+++ queue/python/queue.py
@@ -4,39 +4,39 @@ Queue = {'front': None, 'back': None}
 
 
 class Node:
-  def __init__(self, data, next):
-    self.data = data
-    self.next = next
+    def __init__(self, data, next):
+        self.data = data
+        self.next = next
 
 
-def Enqueue(Queue, element):
-  N = Node(element, None)
-  if Queue['back'] is None:
-    Queue['front'] = N
-    Queue['back'] = N
-  else:
-    Queue['back'].next = N
-    Queue['back'] = Queue['back'].next
+    def Enqueue(Queue, element):
+        N = Node(element, None)
+        if Queue['back'] is None:
+            Queue['front'] = N
+            Queue['back'] = N
+        else:
+            Queue['back'].next = N
+            Queue['back'] = Queue['back'].next
 
 
-def Dequeue(Queue):
-  if Queue['front'] is not None:
-    first = Queue['front']
-    Queue['front'] = Queue['front'].next
-    return first.data
-  else:
-    if Queue['back'] is not None:
-      Queue['back'] = None
-    return 'Cannot dequeue because queue is empty'
+    def Dequeue(Queue):
+        if Queue['front'] is not None:
+            first = Queue['front']
+            Queue['front'] = Queue['front'].next
+            return first.data
+        else:
+            if Queue['back'] is not None:
+                Queue['back'] = None
+            return 'Cannot dequeue because queue is empty'
 
 
 def main():
-  Enqueue(Queue,'a')
-  Enqueue(Queue,'b')
-  Enqueue(Queue,'c')
-  print(Dequeue(Queue))
-  print(Dequeue(Queue))
-  print(Dequeue(Queue))
+    Enqueue(Queue, 'a')
+    Enqueue(Queue, 'b')
+    Enqueue(Queue, 'c')
+    print(Dequeue(Queue))
+    print(Dequeue(Queue))
+    print(Dequeue(Queue))
 
 if __name__ == "__main__":
-  main()
+    main()
