b49c37dc919d2ad74f4da5e65c460167f46e6ac2
Alexander Medvedsky <aleksander.medvedskiy@gmail.com>
2018-10-02 20:37:46 +0300

    Delete Bubble_sort.py

--- bubble_sort/Python/Bubble_sort.py
+++
@@ -1,9 +0,0 @@
-def bubble_sort(mas):
-    for i in range(len(mas)):
-        for j in range(len(mas) - 1, i, -1):
-            if mas[j] < mas[j-1]:
-                mas[j], mas[j-1] = mas[j-1], mas[j]
-
-    return mas
-
-print(bubble_sort([4, 3, 2, 1]))
