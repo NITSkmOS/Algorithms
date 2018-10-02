876d9ebba81d77e05606bd34557fd26ade7a5c6c
Aleksandr <aleksander.medvedskiy@gmail.com>
2018-10-02 14:48:08 +0300

    Add Bubble sort on Python

---
+++ bubble_sort/Python/Bubble_sort.py
@@ -0,0 +1,9 @@
+def bubble_sort(mas):
+    for i in range(len(mas)):
+        for j in range(len(mas) - 1, i, -1):
+            if mas[j] < mas[j-1]:
+                mas[j], mas[j-1] = mas[j-1], mas[j]
+
+    return mas
+
+print(bubble_sort([4, 3, 2, 1]))
