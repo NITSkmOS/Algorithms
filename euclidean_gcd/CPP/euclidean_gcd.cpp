3b922d7ff83b9bbbf5e01da5d916a80c48bd947e
Shubham Maurya <code.shaurya@gmail.com>
2018-10-03 01:02:44 +0530

    all issues were resolved

--- euclidean_gcd/CPP/euclidean_gcd.cpp
+++ euclidean_gcd/CPP/euclidean_gcd.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
 
-ll gcd(ll a, ll b)
-{
+int gcd(int a, int b){
     if (a == 0)
         return b;
     if (b == 0)
@@ -11,13 +9,12 @@ ll gcd(ll a, ll b)
     return gcd(b % a, a);
 }
 
-int main()
-{
+int main(){
     // a and b are two numbers.
-    ll a, b;
+    int a, b;
     cin >> a >> b;
 
-    ll ans = gcd(a, b);
+    int ans = gcd(a, b);
     cout << ans;
 
     return 0;
