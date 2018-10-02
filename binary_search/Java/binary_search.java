eb2a206315ca6c39509a7915eb35c5a7d19296f2
Shoumik Dey <shoumikdey123@gmail.com>
2018-10-03 01:15:26 +0530

    Add binary search algorithm in java

--- binary_search/Java/binary_search.java
+++ binary_search/Java/binary_search.java
@@ -2,9 +2,9 @@ class BinarySearch
 {
   public static void main(String[] args)
   {
-    int a[] = {1,2,3,4,5,6,7,8,9, 0};
+    int a[] = {1,2,3,4,5,6,7,8,9, 0}; //intialising the array
     int n = 5;
-    if(binarySearch(a, n) == true)
+    if(binarySearch(a, n) == true) //checking if element is found in the list of the elements
       System.out.println("Element found!");
     else
       System.out.println("Element not found");
@@ -12,10 +12,10 @@ class BinarySearch
   }
 
   static boolean binarySearch(int arr[], int n)
-  {
+  { //function to do a binary search on any array
     int len = arr.length;
-    int l = 0;
-    int u = len-1;
+    int l = 0; //lower limit
+    int u = len-1; //upper limit
     int m = (l + u)/2;
     boolean flag = false;
     while(u >= l)
@@ -23,7 +23,7 @@ class BinarySearch
       if(arr[m] == n)
         flag = true;
       if(arr[m] > n)
-        u = m - 1;
+        u = m - 1; //decrementing lower limit as array is sorted
       else
         l = m + 1;
       m = (l + u)/2;
