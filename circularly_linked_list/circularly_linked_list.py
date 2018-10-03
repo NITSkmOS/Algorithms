b1d917039f0fd90f2a90989057a05044cd2b2ca4
Rishik <rishiksood15@gmail.com>
2018-10-03 23:30:09 +0530

    circularly_linked_list.py Add Circularly Linked List
    Added Circularly Linked List which is a linked list in which the
    nodes are connected in the form of a circle. There is no NULL at
    the end or starting. If we try to go beyond the last element, we
    will return to the first node.
    
    Closes https://github.com/NITSkmOS/Algorithms/issues/33

--- circularly_linked_list/circularly_linked_list.py
+++ circularly_linked_list/circularly_linked_list.py
@@ -1,177 +1,180 @@
+'''
+Circularly Linked List which is a linked list in which the
+nodes are connected in the form of a circle. There is no NULL at
+the end or starting. If we try to go beyond the last element, we
+will return to the first node.
+'''
+
+
 class Node:
-	#Define how each entry will be stored
-	def __init__(self, data):
-		self.data = data
-		self.next = None
-		self.prev = None
-
-class circular_linked_list:
-	def __init__(self):
-		self.head = None
-
-	def insert_new_head(self, data, new_node):
-		self.head = new_node
-		self.head.next = new_node
-		self.head.prev = new_node
-
-	def insert_begin(self, data):
-
-		'''
-		Places an element at the start of the circularly linked list.
-
-		Simulation:
-		(_) _ _ _ _ _ _ 	 _ _ _ _ _ _ _
-		    ^		  |		 ^			 |
-		    |		  |	==>  |           |
-		    |		  |		 |   		 |
-		    |----------	     |------------
-
-		'''
-
-		new_node = Node(data)
-
-		if self.head == None:
-			self.insert_new_head(data, new_node)
-
-		else:
-			new_node.next = self.head.next
-			new_node.prev = self.head
-			self.head.next = new_node
-			self.head.next.prev = new_node
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
 
 
-	def insert_end(self, data):
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
 
-		'''
-		Places an element at the end of the circularly linked list.
+    def insert_new_head(self, new_node):
+        self.head = new_node
+        self.head.next = new_node
+        self.head.prev = new_node
 
-		Simulation:
-		_ _ _ _ _ _ (_)		 _ _ _ _ _ _ _
-		^		  |			 ^			 |
-		|		  |		==>  |           |
-		|		  |		     |   		 |
-		|----------			 |------------
+    def insert_begin(self, data):
+        '''
+        Places an element at the start of the circularly linked list.
 
-		'''
+        Simulation:
+        (_) _ _ _ _ _ _ 	 _ _ _ _ _ _ _
+            ^		  |		 ^			 |
+            |		  |	==>  |           |
+            |		  |		 |   		 |
+            |----------	     |------------
 
+        '''
 
-		new_node = Node(data)
+        new_node = Node(data)
 
-		if self.head == None:
-			self.insert_new_head(data, new_node)
+        if self.head is None:
+            self.insert_new_head(new_node)
 
-		else:
-			new_node.next = self.head.next
-			new_node.prev = self.head
-			self.head.next.prev = new_node
-			self.head.next = new_node
-			self.head = new_node
+        else:
+            new_node.next = self.head.next
+            new_node.prev = self.head
+            self.head.next = new_node
+            self.head.next.prev = new_node
 
-	def remove_head(self):
+    def insert_end(self, data):
+        '''
+        Places an element at the end of the circularly linked list.
 
-		'''
-		Removes the head node.
+        Simulation:
+        _ _ _ _ _ _ (_)		 _ _ _ _ _ _ _
+        ^		  |			 ^			 |
+        |		  |		==>  |           |
+        |		  |		     |   		 |
+        |----------			 |------------
 
-		Simulation:
-		_ _ _ _ _ _(_) 		  _ _ _ _ _ _
-		^		    |		  ^			|
-		|		    |	==>   |         |
-		|			|		  |   		|
-		|------------	 	  |------------
+        '''
 
-		'''
+        new_node = Node(data)
 
-		if self.size() == 0:
-			return None
+        if self.head is None:
+            self.insert_new_head(new_node)
 
-		if self.head.next == self.head:
-			self.head = None 
+        else:
+            new_node.next = self.head.next
+            new_node.prev = self.head
+            self.head.next.prev = new_node
+            self.head.next = new_node
+            self.head = new_node
 
-		else:
-			self.head.prev.next = self.head.next
-			self.head.next.prev = self.head.prev
-			self.head = self.head.prev
+    def remove_head(self):
+        '''
+        Removes the head node.
 
-	def display(self):
+        Simulation:
+        _ _ _ _ _ _(_) 		  _ _ _ _ _ _
+        ^		    |		  ^			|
+        |		    |	==>   |         |
+        |			|		  |   		|
+        |------------	 	  |------------
 
-		'''
-		Displays list from the start to the end.
+        '''
 
-		Simulation:
+        if self.size() == 0:
+            return None
 
-		1 2 3 4 5 6 7 => Order of output
-		_ _ _ _ _ _ _
-		^		    |
-		|		    |
-		|			|
-		|------------
+        if self.head.next == self.head:
+            self.head = None
 
-		'''
+        else:
+            self.head.prev.next = self.head.next
+            self.head.next.prev = self.head.prev
+            self.head = self.head.prev
 
-		if self.size() == 0:
-			return None
+    def display(self):
+        '''
+        Displays list from the start to the end.
 
-		linear_display = []
+        Simulation:
 
-		cur_node = self.head.next
+        1 2 3 4 5 6 7 => Order of output
+        _ _ _ _ _ _ _
+        ^		    |
+        |		    |
+        |			|
+        |------------
 
-		while True:
-			linear_display.append(cur_node.data)
-			if cur_node == self.head:
-				break
-			cur_node = cur_node.next
+        '''
 
-		return linear_display
+        if self.size() == 0:
+            return None
 
-	def clear(self):
+        linear_display = []
 
-		self.head = None
+        cur_node = self.head.next
 
-	def size(self):
+        while True:
+            linear_display.append(cur_node.data)
+            if cur_node == self.head:
+                break
+            cur_node = cur_node.next
 
-		'''
-		Returns size of Circularly Linked List.
+        return linear_display
 
-		Simulation:
+    def clear(self):
 
-		1 2 3 4 5 6(7) => Size
-		_ _ _ _ _ _ _
-		^		    |
-		|		    |
-		|			|
-		|------------
+        self.head = None
 
-		'''
+    def size(self):
+        '''
+        Returns size of Circularly Linked List.
 
-		cur_node = self.head
-		length = 0
+        Simulation:
 
-		while True:
+        1 2 3 4 5 6(7) => Size
+        _ _ _ _ _ _ _
+        ^		    |
+        |		    |
+        |			|
+        |------------
 
-			try:
-				cur_node = cur_node.next
-			except AttributeError:
-				break
+        '''
 
-			length += 1
+        cur_node = self.head
+        length = 0
 
-			if cur_node == self.head:
-				break
+        while True:
 
-		return length
+            try:
+                cur_node = cur_node.next
+            except AttributeError:
+                break
+
+            length += 1
+
+            if cur_node == self.head:
+                break
+
+        return length
+
 
 def main():
-	cll = Circular_Linked_List()
-	cll.remove_head()
-	print cll.display()
-	print cll.size()
-	cll.insert_end(15)
-	cll.insert_end(10)
-	print cll.display()
-	print cll.size()
-	cll.clear()
-	print cll.display()
-	print cll.size()
+    cll = CircularLinkedList()
+    cll.remove_head()
+    print cll.display()
+    print cll.size()
+    cll.insert_end(15)
+    cll.insert_end(10)
+    print cll.display()
+    print cll.size()
+    cll.clear()
+    print cll.display()
+    print cll.size()
+
 
 if __name__ == '__main__':
-	main()
+    main()
