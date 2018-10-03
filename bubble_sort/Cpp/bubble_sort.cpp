693dc5f921b2c67aae0ce9ede03587a8696086d6
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-10-03 22:18:23 +0530

    Update bubble_sort.cpp

--- bubble_sort/Cpp/bubble_sort.cpp
+++ bubble_sort/Cpp/bubble_sort.cpp
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+using namespace std;
 
 void swap(int *xp, int *yp) {
 	int temp = *xp;
@@ -17,10 +19,6 @@ void bubble_sort(int arr[], int n) {
 				swapped = true;
 			}
 		}
-
-		// If no two elements were swapped by inner loop, then break
-		if (swapped == false)
-			break;
 	}
 }
 
@@ -28,17 +26,18 @@ void bubble_sort(int arr[], int n) {
 void print_array(int arr[], int size) {
 	int i;
 	for (i = 0; i < size; i++)
-		printf("%d ", arr[i]);
-
-	printf("\n");
+		cout<<arr[i];
+	cout<<endl;
 }
 
 // Driver program to test above functions
 int main() {
+	clrscr();
 	int arr[] = {64, 34, 25, 12, 22, 11, 90};
 	int n = sizeof(arr) / sizeof(arr[0]);
 	bubble_sort(arr, n);
-	printf("Sorted array: \n");
+	cout<<"Sorted array: "<<endl;
 	print_array(arr, n);
+	getch();
 	return 0;
 }
