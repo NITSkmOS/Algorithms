f697748af4fcfa0c62c1a8d1d293c6e32fca13a6
Sangam Kumar <ksangam68@gmail.com>
2018-10-03 23:45:53 +0530

    Update linked_list.py

--- linked_list/Python/linked_list.py
+++ linked_list/Python/linked_list.py
@@ -1,69 +1,79 @@
 #!/usr/bin/env python3
 
 
+class NoSuchNodeException(Exception):
+    """Exception class"""
+    pass
+
+
+class Node:
+    """
+    Class for the structure of node in Linked List
+
+    :param data:    Data value for node
+    :param next_node:   Next node reference
+    """
+
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    def compare_to(self, node):
+        """
+        Method to compare node data
+
+        :return:    Returns True if node data matches
+        """
+        return node.data == self.data
+
+
 class LinkedList:
     """
     Class for the Linked List
+    :param head:    Reference of the head.
+    :param list_size:   List size.
     """
 
-    class Node:
-        """
-        Class for the structure of
-        node in Linked List
-        """
-
-        next_node = None
-        data = None
-
-        def __init__(self, data, next_node):
-            self.data = data
-            self.next_node = next_node
-
-        def compare_to(self, node):
-            return node.data == self.data
-
-    class NoSuchNodeException(Exception):
-        pass
-
-    head = None
-    list_size = 0
-
-    def __init__(self):
-        pass
+    def __init__(self, head=None, list_size=0):
+        self.head = head
+        self.list_size = list_size
 
     def size(self):
         """
         This methods returns the size of the linked list.
-        :return: The size of the linked list.
+
+        :return:    The size of the linked list.
         """
         return self.list_size
 
     def is_empty(self):
         """
         This methods determines whether the list is empty.
-        :return: True if the list is empty; False otherwise.
+        :return:    True if the list is empty, otherwise False.
         """
         return self.list_size == 0
 
     def get_first(self):
         """
         This method returns the first element of the list.
-        :return: The first element of the list.
+
+        :return:    The first element of the list.
         """
         if self.is_empty():
-            raise self.NoSuchNodeException()
+            raise NoSuchNodeException()
 
         return self.head.data
 
     def getNode(self, i):
         """
         This method takes an index and finds the node residing.
-        Raises an NoSuchNodeException() if no such element.
-        :param i: The index of the node.
-        :return: The ith node of the list.
+
+        :raises:    NoSuchNodeException if no such element.
+        :param i:   The index of the node.
+        :return:    The ith node of the list.
         """
         if i < 0 or i > self.list_size - 1:
-            raise self.NoSuchNodeException()
+            raise NoSuchNodeException()
 
         current = 0
         p = self.head
@@ -77,9 +87,10 @@ class LinkedList:
     def add_first(self, data):
         """
         This method adds a node to the front of the list.
-        :param data: The data of the node to add.
+
+        :param data:    The data of the node to add.
         """
-        node = self.Node(data, self.head)
+        node = Node(data, self.head)
         self.head = node
 
         self.list_size += 1
@@ -87,9 +98,10 @@ class LinkedList:
     def add_last(self, data):
         """
         This method adds a node to the back of the list.
-        :param data: The data of the node to add.
+
+        :param data:    The data of the node to add.
         """
-        node = self.Node(data, None)
+        node = Node(data, None)
 
         if self.is_empty():
             self.head = node
@@ -102,10 +114,11 @@ class LinkedList:
     def remove_first(self):
         """
         This method removes the first element of the list.
-        :return: The value of the first element.
+
+        :return:    The value of the first element.
         """
         if self.is_empty():
-            raise self.NoSuchNodeException()
+            raise NoSuchNodeException()
 
         tmp_val = self.head.data
         self.head = self.head.next_node
@@ -115,10 +128,11 @@ class LinkedList:
     def remove_last(self):
         """
         This method removes the last element of the list.
-        :return: The value of the removed element.
+
+        :return:    The value of the removed element.
         """
         if self.is_empty():
-            raise self.NoSuchNodeException()
+            raise NoSuchNodeException()
 
         tail = self.getNode(self.list_size - 1)
         tail_data = tail.data
@@ -130,12 +144,11 @@ class LinkedList:
             before_tail.next_node = None
 
         self.list_size -= 1
-
         return tail_data
 
     def print_list(self):
         """
-        This method prints a list which a newline after each element.
+        Method to print the list in a newline after each element.
         """
         p = self.head
         i = 0
