b0fc119d3a5edefbff6337a415f83663c437cb24
Geraldo Castro <geraldo.castro@evolux.net.br>
2018-10-03 18:52:16 -0300

    Refactor Stack code

--- stack/c++/stack.cpp
+++ stack/c++/stack.cpp
@@ -9,49 +9,39 @@ class Stack {
 public: 
     int a[MAX];    //Maximum size of Stack 
   
-    Stack()  { top = -1; } 
+    Stack() { top = -1; } 
     bool push(int x); 
     int pop(); 
     bool isEmpty(); 
 }; 
   
-bool Stack::push(int x) 
-{ 
-    if (top >= (MAX-1)) 
-    { 
+bool Stack::push(int x) { 
+    if (top >= (MAX-1)) { 
         cout << "Stack Overflow"; 
         return false; 
-    } 
-    else
-    { 
+    } else { 
         a[++top] = x; 
-        cout<<x <<" pushed into stack\n"; 
+        cout << x << " pushed into stack\n"; 
         return true; 
     } 
 } 
   
-int Stack::pop() 
-{ 
-    if (top < 0) 
-    { 
+int Stack::pop() { 
+    if (top < 0) { 
         cout << "Stack Underflow"; 
         return 0; 
-    } 
-    else
-    { 
+    } else { 
         int x = a[top--]; 
         return x; 
     } 
 } 
   
-bool Stack::isEmpty() 
-{ 
+bool Stack::isEmpty() { 
     return (top < 0); 
 } 
   
 
-int main() 
-{ 
+int main() { 
     struct Stack s; 
     s.push(10); 
     s.push(20); 
