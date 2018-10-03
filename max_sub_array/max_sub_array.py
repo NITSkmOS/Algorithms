beb01d0a3e3b4825afdd49078ae42bc75ecb617a
uday kiran <udaykiran.kondreddy@gmail.com>
2018-10-03 21:22:21 +0530

    Add Python Max Sub array

--- max_sub_array/max_sub_array.py
+++ max_sub_array/max_sub_array.py
@@ -1,4 +1,6 @@
 def max_sub_array_sum(a, size):
+
+
     max_so_far = -9999999 - 1
     max_ending_here = 0
     for i in range(0, size):
@@ -8,5 +10,7 @@ def max_sub_array_sum(a, size):
         if max_ending_here < 0:
             max_ending_here = 0
     return max_so_far
+
+
 a = [-2, -3, 4, -1, -2, 1, 5, -3]
 print("Maximum contiguous sum is", max_sub_array_sum(a, len(a)))
