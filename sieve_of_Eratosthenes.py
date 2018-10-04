e74ef6ec2ba3080756c2964029eed13bb51ba27a
Uday Kiran <udaykiran.kondreddy@gmail.com>
2018-10-04 12:37:17 +0530

    sieve of eratosthenes python code

---
+++ sieve_of_Eratosthenes.py
@@ -0,0 +1,22 @@
+def sieve_of_eratosthenes(n):
+    
+    prime = [True for i in range(n+1)]
+    p = 2
+    while (p * p <= n):
+        # If prime[p] is not changed, then it is a prime
+        if (prime[p] == True):
+            # Update all multiples of p
+            for i in range(p * 2, n+1, p):
+                prime[i] = False
+        p += 1
+    # Print all prime numbers
+    for p in range(2, n):
+        if prime[p]:
+            print(p)
+
+
+if __name__=='__main__':
+    n = 30
+    print("Following are the prime numbers smaller")
+    print("than or equal to", n)
+    sieve_of_eratosthenes(n)
