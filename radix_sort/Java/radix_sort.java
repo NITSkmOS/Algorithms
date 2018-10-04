91c910d5f4a5a3582fbcbcf7fa61be15b55ac5f1
Alexander Medvedsky <aleksander.medvedskiy@gmail.com>
2018-10-04 10:09:03 +0300

    Update radix_sort.java

--- radix_sort/Java/radix_sort.java
+++ radix_sort/Java/radix_sort.java
@@ -1,37 +1,27 @@
-package com.javatutorial;
-import java.util.ArrayList;
-import java.util.List;
-public class ExampleRadixSort {
+public class RadixSort {
+	public static void sort( int[] a) {
+        int i, m = a[0], exp = 1, n = a.length;
+        int[] b = new int[n];
+        for (i = 1; i < n; i++)
+            if (a[i] > m)
+                m = a[i];
+        while (m / exp > 0) {
+            int[] bucket = new int[10];
+            for (i = 0; i < n; i++)
+                bucket[(a[i] / exp) % 10]++;
+            for (i = 1; i < 10; i++)
+                bucket[i] += bucket[i - 1];
+            for (i = n - 1; i >= 0; i--)
+                b[--bucket[(a[i] / exp) % 10]] = a[i];
+            for (i = 0; i < n; i++)
+                a[i] = b[i];
+            exp *= 10;        
+        }
+    }    
     public static void main(String[] args) {
         int[] num = {170, 45, 75, 90, 802, 2, 24, 66,23,234,3,232,44};
-        radixsort(num);
+        sort(num);
         for (int h : num)
-            System.out.print(h + " ");
-    }
-    public static void radixsort(int[] input) {
-        List<Integer>[] buckets = new ArrayList[10];
-        for (int i = 0; i < buckets.length; i++) {
-            buckets[i] = new ArrayList<Integer>();
-        }
-        boolean flag = false;
-        int tmp = -1, divisor = 1;
-        while (!flag) {
-            flag = true;
-          for (Integer i : input) {
-            tmp = i / divisor;
-            buckets[tmp % 10].add(i);
-            if (flag && tmp > 0) {
-                flag = false;
-            }
-          }
-          int a = 0;
-          for (int b = 0; b < 10; b++) {
-            for (Integer i : buckets[b]) {
-              input[a++] = i;
-            }
-            buckets[b].clear();
-          }
-          divisor *= 10;
-        }
+            System.out.print(h + " ");                     
     }
 }
