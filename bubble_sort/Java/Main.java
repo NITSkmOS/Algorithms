c0a3abb875143532c9cc5657c41c471038ea4c83
saboor <saboor.elahi.4@gmail.com>
2018-10-04 19:31:02 +0500

    Main method modified

--- bubble_sort/Java/Main.java
+++ bubble_sort/Java/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
 
