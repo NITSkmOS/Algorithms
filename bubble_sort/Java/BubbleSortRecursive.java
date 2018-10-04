a5398acf1544ff6e3336db07ab796d565a417d3e
sameerchoubey <sameer.choubey98@gmail.com>
2018-10-04 16:44:21 +0530

    BubbleSortRecursive.java: Add BubbleSort Algo
    This adds the recursive implementation of BubbleSort Algorithm in Java.
    The time complexity of the algorithm is O(n^2).
    
    Closes https://github.com/NITSkmOS/Algorithms/issues/18

---
+++ bubble_sort/Java/BubbleSortRecursive.java
@@ -0,0 +1,40 @@
+public class BubbleSortRecursive {
+    public static void main(String[] args){
+    //Taking a sample array for sorting it.
+        int[] nums = {12, 45, 23, 78, 67};
+
+    //calling the method and passing the array in it.
+        bubble(nums, nums.length);
+    //Printing the sorted array
+        printSortedArray(nums);
+    }
+    //The recurisve method that takes in the array and the length of it.
+    public static void bubble(int[] nums, int end){
+    //base case
+        if (end == 1) {
+            return;
+        }
+    //for loop that iterates and sorts the sorts the array
+        for (int i = 0; i < end - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                int temp = nums[i];
+                nums[i] = nums[i + 1];
+                nums[i + 1] = temp;
+            }
+        }
+    //calling the method again to sort the rest of the elements on next iteration
+        bubble(nums, end - 1);
+    }
+
+    //Method to print an array
+    public static void printSortedArray(int arr[]) {
+
+    //storing the length of an array using the .length function in Java
+        int n = arr.length;
+    //For loop that iterates over each element of the array and then prints it.
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+}
