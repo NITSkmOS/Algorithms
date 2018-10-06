e2f77c3082282eb22b86771ad8cff5ac194ad35e
Sartech-B <sarthakbawal@gmail.com>
2018-10-06 21:41:50 +0400

    Minor PR issues

--- knapsack/Python/knapsack.py
+++ knapsack/Python/knapsack.py
@@ -3,8 +3,10 @@ def knapsack(v, w, n, W):
     Function solves 0/1 Knapsack problem
     Function to calculate frequency of items to be added to knapsack to maximise value
     such that total weight is less than or equal to W
-    :param v: set of values of n objects, first element is useless because relevant values are assumed to begin from index 1
-    :param w: set of weights of n objects, first element is useless because relevant weights are assumed to begin from index 1
+    :param v: set of values of n objects,
+    :param w: set of weights of n objects,
+    first element in v, w is useless
+    relevant values start from index 1
     :param n: number of items
     :pram W: maximum weight allowd by knapsack
     """
@@ -27,7 +29,7 @@ def main():
     w = [0, 5, 6, 2, 3, 4]
     n = 5
     W = 8
-    print("Maximum value possible in given weight limit: ", 
+    print("Maximum value possible in given weight limit: ",
         str(knapsack(v, w, n, W)))
 
 
