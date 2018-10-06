732e3bb40bfc32055ec7ae56eb1af1c963bb32cb
Vivek Kumar <vivek.ku1997@gmail.com>
2018-10-06 12:44:07 +0530

    Added whitespaces

--- floyd_warshall_algorithm/CPP/floyd-warshall algorithm.cpp
+++ floyd_warshall_algorithm/CPP/floyd-warshall algorithm.cpp
@@ -5,50 +5,42 @@ using namespace std;
 
 #define INF 999999999
 
-int main()
-{
-	int graph[1001][1001],i,j,k,n,m;
-	cin>>n>>m;	//number of vertices and edges respectively
+int main() {
+	int graph[1001][1001], i, j, k, n, m;
+	cin >> n >> m;	//number of vertices and edges respectively
 
-	for(i=1;i<=n;i++)
-	{
-		for(j=1;j<=n;j++)
-			if(i==j)	graph[i][j]=0;	//Shortest distance between (i,i) is 0
+	for(i = 1; i <= n; i++) {
+		for(j = 1; j <= n; j++)
+			if(i == j)	graph[i][j] = 0;	//Shortest distance between (i,i) is 0
 			else
 				graph[i][j] = INF;		//Initializing other distances as infinite
 	}
 
-	for(i=1;i<=m;i++)
-	{
-		int u,v,w;
-		cin>>u>>v>>w;
+	for(i = 1; i <= m; i++) {
+		int u, v, w;
+		cin>> u >> v >> w;
 		graph[u][v] = w;				//input graph
 		graph[v][u] = w;				//Shortest distance between (i,j) using atmost one edge
 	}
 
 
-	for(k=1;k<=n;k++)					
-	{
-		for(int u=1;u<=n;u++)
-		{
-			for(int v=1;v<=n;v++)
-			{
+	for(k = 1; k <= n; k++) {
+		for(int u = 1; u <= n; u++) {
+			for(int v = 1;v <= n; v++) {
 				//Shortest distance between (i,j) using atmost k edges
-				graph[u][v] = min(graph[u][v],graph[u][k]+graph[k][v]);	
+				graph[u][v] = min(graph[u][v], graph[u][k] + graph[k][v]);	
 			}
 		}
 	}
 
 	//Resulting all pair shortest path in given graph
-	for(int u=1;u<=n;u++)
-	{
-		for(int v=1;v<=n;v++)
-		{
-			cout<<graph[u][v]<<" ";
+	for(int u = 1; u <= n; u++) {
+		for(int v = 1; v <= n; v++) {
+			cout << graph[u][v] << " ";
 		}
-		cout<<endl;
+		cout << endl;
 	}
-	cout<<endl;
+	cout << endl;
 	return 0;
 }
 
