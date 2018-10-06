12a997264a02352f96b955895ee7e4b9c5ba4c25
Pratok-Sanghani <pratiksanghani70@gmail.com>
2018-10-07 01:32:49 +0530

    changes in bubblesort

--- bubble_sort/Python/bubble_sort.py
+++ bubble_sort/Python/bubble_sort.py
@@ -4,12 +4,11 @@ def bubbleSort(arr):
     for i in range(n):
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
-                arr[j],arr[j+1] = arr[j+1],arr[j]
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
-a = [76, 74, 63, 74, 24, 93, 25]
+a = [76,74,63,74,24,93,25]
 
 bubbleSort(a)
 
 print ("Sorted array is:")
 print(a)
-  
