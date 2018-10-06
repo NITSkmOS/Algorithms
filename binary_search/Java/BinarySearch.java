7db8ba9caf6589fb093ef8ac351d4a5bb8889d4b
Ruchika Salwan <ruchikasalwan80@gmail.com>
2018-10-06 23:16:50 +0530

    Updated spacing

--- binary_search/Java/BinarySearch.java
+++ binary_search/Java/BinarySearch.java
@@ -6,27 +6,27 @@ import java.util.Arrays;
  */
 public class BinarySearch {
 
-	public static void main(String[] args) {
-		// write your code here
-		int[] nums = { 3, 4, 1, 2, 5 };
-		int key = 5;
-		Arrays.sort(nums);
-		System.out.println(findIndex(nums, key) + 1);
-	}
+    public static void main(String[] args) {
+        // write your code here
+        int[] nums = { 3, 4, 1, 2, 5 };
+        int key = 5;
+        Arrays.sort(nums);
+        System.out.println(findIndex(nums, key) + 1);
+    }
 
-	private static int findIndex(int[] nums, int key) {
-		int index = -1;
-		int low = 0;
-		int high = nums.length - 1;
-		while (low <= high) {
-			int mid = (low + high) / 2;
-			if (nums[mid] == key)
-				return mid;
-			else if (nums[mid] > key)
-				high = mid - 1;
-			else
-				low = mid + 1;
-		}
-		return index;
-	}
+    private static int findIndex(int[] nums, int key) {
+        int index = -1;
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == key)
+                return mid;
+            else if (nums[mid] > key)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return index;
+    }
 }
