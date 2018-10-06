9aca31ca82faacad029d6509386a32752f9e1445
PinkiePie1189 <mircea_costin84@yahoo.com>
2018-10-06 22:02:08 +0300

    heap sort in C

---
+++ heap_sort/c/heapsort.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include "heap.h"
+#define MAXN 100
+
+void heapsort(int* v,int len);
+int main()
+{
+    //Driver program
+    int n;
+    scanf("%d",&n);
+    int v[MAXN];
+    int i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&v[i]);
+    }
+
+    heapsort(v,n);
+
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",v[i]);
+    }
+    return 0;
+}
+
+void heapsort(int* v,int len)
+{
+    MinHeap aux;
+
+
+    for(int i=0;i<len;i++)
+    {
+        add(&aux,v[i]);
+    }
+
+    for(int i=0;i<len;i++)
+    {
+        v[i]=get_value_at_node(&aux,1);
+        rmv(&aux,1);
+    }
+}
