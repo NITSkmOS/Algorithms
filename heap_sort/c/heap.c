a7d3073a51e06d2ebd88d79ea58e6770c75795cb
PinkiePie1189 <mircea_costin84@yahoo.com>
2018-10-07 14:25:31 +0300

    Fixed elses and commas

--- heap_sort/c/heap.c
+++ heap_sort/c/heap.c
@@ -2,44 +2,42 @@
 
 
 
-void add(MinHeap* h, int value){
-	h->heap[++(h->N)]=value;
+void add(MinHeap* h, int value) {
+	h->heap[++(h->N)] = value;
 
-	sift(h,h->N);
-	percolate(h,h->N);
+	sift(h, h->N);
+	percolate(h, h->N);
 }
 
-void rmv(MinHeap* h, int node){
-	swap_nodes(h,node,h->N);
+void rmv(MinHeap* h, int node) {
+	swap_nodes(h, node, h->N);
 	h->N--;
 
-	sift(h,node);
-	percolate(h,node);
+	sift(h, node);
+	percolate(h, node);
 }
 
-void sift(MinHeap* h, int node){
+void sift(MinHeap* h, int node) {
 	int son;
 
 	do{
-		son=0;
-		if(2*node+1 <= h->N){
-			if(h->heap[2*node+1] < h->heap[2*node]){
-				son=2*node+1;
-			}
-			else{
+		son = 0;
+		if(2*node+1 <= h->N) {
+			if (h->heap[2*node+1] < h->heap[2*node]) {
+				son = 2*node+1;
+			} else {
 				son=2*node;
 			}
-		}
-		else if(2*node <= h->N){
+		} else if (2*node <= h->N) {
 			son=2*node;
 		}
 
-		if(h->heap[node] < h->heap[son]){
+		if(h->heap[node] < h->heap[son]) {
 			son=0;
 		}
 
-		if(son){
-			swap_nodes(h,node,son);
+		if(son) {
+			swap_nodes(h, node, son);
 			node=son;
 		}
 	}
@@ -47,8 +45,8 @@ void sift(MinHeap* h, int node){
 }
 
 void percolate(MinHeap* h, int node){
-	while(node > 1 && h->heap[node] < h->heap[node/2]){
-		swap_nodes(h,node,node/2);
+	while(node > 1 && h->heap[node] < h->heap[node/2]) {
+		swap_nodes(h, node, node/2);
 		node/=2;
 	}
 }
--- heap_sort/c/heapsort.c
+++ heap_sort/c/heapsort.c
@@ -3,35 +3,34 @@
 #define MAXN 100
 
 void heapsort(int* v, int len);
-int main()
-{
+int main() {
 	//Driver program
 	int n;
-	scanf("%d",&n);
+	scanf("%d", &n);
 	int v[MAXN];
 	int i;
-	for(i=0; i < n; i++){
-		scanf("%d",&v[i]);
+	for(i=0; i < n; i++) {
+		scanf("%d", &v[i]);
 	}
 
-	heapsort(v,n);
+	heapsort(v, n);
 
-	for(i=0; i < n; i++){
-		printf("%d ",v[i]);
+	for(i=0; i < n; i++) {
+		printf("%d ", v[i]);
 	}
 	return 0;
 }
 
-void heapsort(int* v, int len){
+void heapsort(int* v, int len) {
 	MinHeap aux;
 
 	int i;
-	for(i=0; i < len; i++){
-		add(&aux,v[i]);
+	for(i=0; i < len; i++) {
+		add(&aux, v[i]);
 	}
 
-	for(i=0; i < len; i++){
-		v[i]=get_value_at_node(&aux,1);
-		rmv(&aux,1);
+	for(i=0; i < len; i++) {
+		v[i] = get_value_at_node(&aux, 1);
+		rmv(&aux, 1);
 	}
 }
