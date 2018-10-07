d58ad0e70904a9c7a31063fc992b907ee42cd845
Vishnu Bochiwal <32838570+visnu-bochiwal@users.noreply.github.com>
2018-10-08 02:08:10 +0530

    indentation corrected.

--- quicksort/c/quicksort.c
+++ quicksort/c/quicksort.c
@@ -1,55 +1,55 @@
 /*
-Quicksort is a divide and conquer algorithm. The steps are: 
-1) Pick an element from the array, this element is called as pivot element. 
-2) Divide the unsorted array of elements in two arrays with values less than the pivot come in the first sub array, while all elements with values greater than the pivot come in the second sub-array (equal values can go either way). This step is called the partition operation. 
-3) Recursively repeat the step 2(until the sub-arrays are sorted) to the sub-array of elements with smaller values and separately to the sub-array of elements with greater values. 
+Quicksort is a divide and conquer algorithm. The steps are:
+1) Pick an element from the array, this element is called as pivot element.
+2) Divide the unsorted array of elements in two arrays with values less than the pivot come in the first sub array, while all elements with values greater than the pivot come in the second sub-array (equal values can go either way). This step is called the partition operation.
+3) Recursively repeat the step 2(until the sub-arrays are sorted) to the sub-array of elements with smaller values and separately to the sub-array of elements with greater values.
 The same logic I have implemented in the following C program.
 */
 
 #include<stdio.h>
 void quicksort(int number[25], int first, int last) {
-  int i, j, pivot, temp;
+	int i, j, pivot, temp;
 
-  if (first < last) {
-    pivot = first;
-    i = first;
-    j = last;
+	if (first < last) {
+		pivot = first;
+  	i = first;
+  	j = last;
 
-    while (i < j) {
-      while (number[i] <= number[pivot] && i < last)
-        i++;
-      while (number[j] > number[pivot])
-        j--;
-      if (i < j) {
-        temp = number[i];
-        number[i] = number[j];
-        number[j] = temp;
-      }
-    }
+		while (i < j) {
+			while (number[i] <= number[pivot] && i < last)
+				i++;
+			while (number[j] > number[pivot])
+				j--;
+			if (i < j) {
+				temp = number[i];
+				number[i] = number[j];
+				number[j] = temp;
+			}
+		}
 
-    temp = number[pivot];
-    number[pivot] = number[j];
-    number[j] = temp;
-    quicksort(number, first, j-1);
-    quicksort(number, j+1, last);
-  }
+		temp = number[pivot];
+		number[pivot] = number[j];
+		number[j] = temp;
+		quicksort(number, first, j-1);
+		quicksort(number, j+1, last);
+	}
 }
 
 int main() {
-  int i, count, number[25];
+	int i, count, number[25];
 
-  printf("How many elements are u going to enter?: ");
-  scanf("%d", &count);
+	printf("How many elements are u going to enter?: ");
+	scanf("%d", &count);
 
-  printf("Enter %d elements: ", count);
-  for (i = 0; i < count; i++)
-    scanf("%d", &number[i]);
+	printf("Enter %d elements: ", count);
+	for (i = 0; i < count; i++)
+		scanf("%d", &number[i]);
 
-  quicksort(number, 0, count-1);
+	quicksort(number, 0, count-1);
 
-  printf("Order of Sorted elements: ");
-  for (i=0; i < count; i++)
-    printf(" %d", number[i]);
+	printf("Order of Sorted elements: ");
+	for (i=0; i < count; i++)
+		printf(" %d", number[i]);
 
-  return 0;
+	return 0;
 }
