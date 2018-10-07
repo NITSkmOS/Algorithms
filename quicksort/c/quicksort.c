0022ae1f71d045691dbb000b72817cf5ea169a6e
Vishnu Bochiwal <32838570+visnu-bochiwal@users.noreply.github.com>
2018-10-08 02:00:10 +0530

    corrected typo

--- quicksort/c/quicksort.c
+++ quicksort/c/quicksort.c
@@ -7,50 +7,49 @@ The same logic I have implemented in the following C program.
 */
 
 #include<stdio.h>
-void quicksort(int number[25],int first,int last){
-   int i, j, pivot, temp;
+void quicksort(int number[25], int first, int last) {
+  int i, j, pivot, temp;
 
-   if(first<last){
-      pivot=first;
-      i=first;
-      j=last;
+  if (first < last) {
+    pivot = first;
+    i = first;
+    j = last;
 
-      while(i<j){
-         while(number[i]<=number[pivot]&&i<last)
-            i++;
-         while(number[j]>number[pivot])
-            j--;
-         if(i<j){
-            temp=number[i];
-            number[i]=number[j];
-            number[j]=temp;
-         }
+    while (i < j) {
+      while (number[i] <= number[pivot] && i < last)
+        i++;
+      while (number[j] > number[pivot])
+        j--;
+      if (i < j) {
+        temp = number[i];
+        number[i] = number[j];
+        number[j] = temp;
       }
+    }
 
-      temp=number[pivot];
-      number[pivot]=number[j];
-      number[j]=temp;
-      quicksort(number,first,j-1);
-      quicksort(number,j+1,last);
-
-   }
+    temp = number[pivot];
+    number[pivot] = number[j];
+    number[j] = temp;
+    quicksort(number, first, j-1);
+    quicksort(number, j+1, last);
+  }
 }
 
-int main(){
-   int i, count, number[25];
+int main() {
+  int i, count, number[25];
 
-   printf("How many elements are u going to enter?: ");
-   scanf("%d",&count);
+  printf("How many elements are u going to enter?: ");
+  scanf("%d", &count);
 
-   printf("Enter %d elements: ", count);
-   for(i=0;i<count;i++)
-      scanf("%d",&number[i]);
+  printf("Enter %d elements: ", count);
+  for (i = 0; i < count; i++)
+    scanf("%d", &number[i]);
 
-   quicksort(number,0,count-1);
+  quicksort(number, 0, count-1);
 
-   printf("Order of Sorted elements: ");
-   for(i=0;i<count;i++)
-      printf(" %d",number[i]);
+  printf("Order of Sorted elements: ");
+  for (i=0; i < count; i++)
+    printf(" %d", number[i]);
 
-   return 0;
+  return 0;
 }
