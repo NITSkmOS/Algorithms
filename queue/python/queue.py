57b838bfa2be80e8ca4f68a2fc3749b51ef6dd14
Md Inzamamul Haque <RLE0594@rlpl.com>
2018-10-08 02:30:30 +0530

    Reafactored the code

--- queue/python/queue.py
+++ queue/python/queue.py
@@ -1,15 +1,12 @@
 # Implementation of queue using Linked List:
-
 class Node:
     def __init__(self, data):
        self.data = data
        self.next = None
- 
 class Queue:
     def __init__(self):
         self.head = None
         self.last = None
- 
     def enqueue(self, data):
         if self.last is None:
             self.head = Node(data)
@@ -17,7 +14,6 @@ class Queue:
         else:
             self.last.next = Node(data)
             self.last = self.last.next
- 
     def dequeue(self):
         if self.head is None:
             return None
@@ -25,15 +21,13 @@ class Queue:
             to_return = self.head.data
             self.head = self.head.next
             return to_return
- 
-a_queue = Queue()
+ a_queue = Queue()
 while True:
     print('enqueue <value>')
     print('dequeue')
     print('quit')
     do = input('What would you like to do? ').split()
- 
-    operation = do[0].strip().lower()
+     operation = do[0].strip().lower()
     if operation == 'enqueue':
         a_queue.enqueue(int(do[1]))
     elif operation == 'dequeue':
