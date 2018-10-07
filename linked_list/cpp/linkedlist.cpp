abafb42aaa41670e08bae392e4f934f008cf0f40
Rajeev R Menon <rajeevrmenon97@live.com>
2018-10-08 00:17:23 +0530

    linkedlist.cpp: Remove space inconsistencies
    This commit removes the space inconsistencies detected by coala
    
    Closes https://github.com/NITSkmOS/Algorithms/issues/369

--- linked_list/cpp/linkedlist.cpp
+++ linked_list/cpp/linkedlist.cpp
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-// A linked list node
 
-struct Node
-{
-  int data;
-  struct Node *next;
+// A linked list node
+struct Node{
+	int data;
+	struct Node *next;
 };
 /*
 Given a reference (pointer to pointer) to the head of a list
 and an int, inserts a new node on the front of the list.
 */
 
-void push(struct Node** head_ref, int new_data)
-{
-  struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
-  new_node->data = new_data;
-  new_node->next = (*head_ref);
-  (*head_ref) = new_node;
+void push(struct Node** head_ref, int new_data){
+	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+	new_node->data = new_data;
+	new_node->next = (*head_ref);
+	(*head_ref) = new_node;
 }
 
 /*
@@ -25,63 +23,60 @@ Given a reference (pointer to pointer) to the head of a list
 and a position, deletes the node at the given position
 */
 
-void deleteNode(struct Node **head_ref, int position)
-{
-  // If linked list is empty
-  if (*head_ref == NULL)
-    return;
-  // Store head node
-  struct Node* temp = *head_ref;
-  // If head needs to be removed
-  if (position == 0)
-  {
-    *head_ref = temp->next;
-    free(temp);
-    return;
-  }
+void deleteNode(struct Node **head_ref, int position){
+	// If linked list is empty
+	if (*head_ref == NULL)
+		return;
+	// Store head node
+	struct Node* temp = *head_ref;
+	// If head needs to be removed
+	if (position == 0){
+		*head_ref = temp->next;
+		free(temp);
+		return;
+	}
 
-  // Find previous node of the node to be deleted
-  for (int i = 0; temp != NULL && i < position - 1; i++)
-    temp = temp->next;
-  // If position is more than number of ndoes
-  if (temp == NULL || temp->next == NULL)
-    return;
-  // Node temp->next is the node to be deleted
-  // Store pointer to the next of node to be deleted
-  struct Node *next = temp->next->next;
+	// Find previous node of the node to be deleted
+	for (int i = 0; temp != NULL && i < position - 1; i++)
+		temp = temp->next;
 
-  // Unlink the node from linked list
-  free(temp->next);
-  temp->next = next;
+	// If position is more than number of ndoes
+	if (temp == NULL || temp->next == NULL)
+		return;
+
+	// Node temp->next is the node to be deleted
+	// Store pointer to the next of node to be deleted
+	struct Node *next = temp->next->next;
+
+	// Unlink the node from linked list
+	free(temp->next);
+	temp->next = next;
 }
 
 // This function prints contents of linked list starting from
 // the given node
-void printList(struct Node *node)
-{
-  while (node != NULL)
-  {
-    printf(" %d ", node->data);
-    node = node->next;
-  }
+void printList(struct Node *node){
+	while (node != NULL){
+		printf(" %d ", node->data);
+		node = node->next;
+	}
 }
 
 /* Drier program to test above functions*/
-int main()
-{
-  /* Start with the empty list */
-  struct Node* head = NULL;
+int main(){
+	/* Start with the empty list */
+	struct Node* head = NULL;
 
-  push(&head, 7);
-  push(&head, 1);
-  push(&head, 3);
-  push(&head, 2);
-  push(&head, 8);
+	push(&head, 7);
+	push(&head, 1);
+	push(&head, 3);
+	push(&head, 2);
+	push(&head, 8);
 
-  puts("Created Linked List: ");
-  printList(head);
-  deleteNode(&head, 4);
-  puts("\nLinked List after Deletion at position 4: ");
-  printList(head);
-  return 0;
+	puts("Created Linked List: ");
+	printList(head);
+	deleteNode(&head, 4);
+	puts("\nLinked List after Deletion at position 4: ");
+	printList(head);
+	return 0;
 }
