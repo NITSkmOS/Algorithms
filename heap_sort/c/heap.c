17058c10df354325cd282837d0d75d566c191b7f
PinkiePie1189 <mircea_costin84@yahoo.com>
2018-10-07 14:32:12 +0300

    Fixed more indentation issues

--- heap_sort/c/heap.c
+++ heap_sort/c/heap.c
@@ -1,4 +1,4 @@
-#include "heap.h"
+#include "heap_sort/c/heap.h"
 
 
 
@@ -20,43 +20,42 @@ void rmv(MinHeap* h, int node) {
 void sift(MinHeap* h, int node) {
 	int son;
 
-	do{
+	do {
 		son = 0;
 		if(2*node+1 <= h->N) {
 			if (h->heap[2*node+1] < h->heap[2*node]) {
 				son = 2*node+1;
 			} else {
-				son=2*node;
+				son = 2*node;
 			}
 		} else if (2*node <= h->N) {
-			son=2*node;
+			son = 2*node;
 		}
 
 		if(h->heap[node] < h->heap[son]) {
-			son=0;
+			son = 0;
 		}
 
 		if(son) {
 			swap_nodes(h, node, son);
-			node=son;
+			node = son;
 		}
-	}
-	while(son);
+	} while(son);
 }
 
-void percolate(MinHeap* h, int node){
+void percolate(MinHeap* h, int node) {
 	while(node > 1 && h->heap[node] < h->heap[node/2]) {
 		swap_nodes(h, node, node/2);
 		node/=2;
 	}
 }
 
-void swap_nodes(MinHeap* h, int node1, int node2){
-	int man=h->heap[node1];
-	h->heap[node1]=h->heap[node2];
-	h->heap[node2]=man;
+void swap_nodes(MinHeap* h, int node1, int node2) {
+	int man = h->heap[node1];
+	h->heap[node1] = h->heap[node2];
+	h->heap[node2] = man;
 }
 
-int get_value_at_node(MinHeap* h, int node){
+int get_value_at_node(MinHeap* h, int node) {
 	return h->heap[node];
 }
