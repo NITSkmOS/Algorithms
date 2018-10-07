d96c403f293082d1be3f0811e1b0e88ff1488ba0
Akash Raj <akashraj7713@gmail.com>
2018-10-07 22:05:02 +0530

    updated

--- bubble_sort/Python/Bubblesort.py
+++ bubble_sort/Python/Bubblesort.py
@@ -2,10 +2,8 @@ def Bubble_sort(arr):
     for j in range(n-1,0,-1):
         for i in range (j):
             if arr[i]>arr[i+1]:
-                arr[i],arr[i+1]=arr[i+1],arr[i] //swapping elements 
-                
+                arr[i],arr[i+1]=arr[i+1],arr[i] //swapping elements
 arr=[45,78,3,45,76,78,23]
 n=len(arr)
 Bubble_sort(arr)
 print(arr)
-            
