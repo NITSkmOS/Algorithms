8a811739ac1b35c51d4ddaad559edadba4d4c57e
PinkiePie1189 <mircea_costin84@yahoo.com>
2018-10-07 15:18:36 +0300

    Merged everything in a single source file

--- heap_sort/c/heapsort.c
+++ heap_sort/c/heapsort.c
@@ -1,6 +1,24 @@
 #include<stdio.h>
-#include <heap_sort/c/heap.h>
 #define MAXN 100
+#define MAX_NODES 100
+typedef struct
+{
+    int N;
+    int heap[MAX_NODES];
+} MinHeap;
+
+void add(MinHeap* h, int value);
+
+void rmv(MinHeap* h, int node);
+
+void sift(MinHeap* h, int node);
+
+void percolate(MinHeap* h, int node);
+
+void swap_nodes(MinHeap* h, int node1, int node2);
+
+
+int get_value_at_node(MinHeap* h, int node);
 
 void heapsort(int* v, int len);
 int main() {
@@ -33,4 +51,62 @@ void heapsort(int* v, int len) {
 		v[i] = get_value_at_node(&aux, 1);
 		rmv(&aux, 1);
 	}
+}
+
+void add(MinHeap* h, int value) {
+	h->heap[++(h->N)] = value;
+
+	sift(h, h->N);
+	percolate(h, h->N);
+}
+
+void rmv(MinHeap* h, int node) {
+	swap_nodes(h, node, h->N);
+	h->N--;
+
+	sift(h, node);
+	percolate(h, node);
+}
+
+void sift(MinHeap* h, int node) {
+	int son;
+
+	do {
+		son = 0;
+		if(2*node+1 <= h->N) {
+			if (h->heap[2*node+1] < h->heap[2*node]) {
+				son = 2*node+1;
+			} else {
+				son = 2*node;
+			}
+		} else if (2*node <= h->N) {
+			son = 2*node;
+		}
+
+		if(h->heap[node] < h->heap[son]) {
+			son = 0;
+		}
+
+		if(son) {
+			swap_nodes(h, node, son);
+			node = son;
+		}
+	} while (son);
+}
+
+void percolate(MinHeap* h, int node) {
+	while(node > 1 && h->heap[node] < h->heap[node/2]) {
+		swap_nodes(h, node, node/2);
+		node/=2;
+	}
+}
+
+void swap_nodes(MinHeap* h, int node1, int node2) {
+	int man = h->heap[node1];
+	h->heap[node1] = h->heap[node2];
+	h->heap[node2] = man;
+}
+
+int get_value_at_node(MinHeap* h, int node) {
+	return h->heap[node];
 }
