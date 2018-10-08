16f66b1743b0db37a7e65d1f3ba661673943ebb4
Nipun Sampath <nipunsampath@gmail.com>
2018-10-09 04:44:57 +0530

    improved code formatting

--- queue/C/main.c
+++ queue/C/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "Queue-Contiguous.h"
+
 int main() {
     Queue myQueue;
     int temp,i;
@@ -8,20 +9,20 @@ int main() {
 
     printf("Is the Queue Empty? \n%s", isQueueEmpty(&myQueue) ? "true" : "false");
 
-    append(7,&myQueue);
-    append(5,&myQueue);
-    append(2,&myQueue);
-    append(3,&myQueue);
-    append(50,&myQueue);
+    append(7, &myQueue);
+    append(5, &myQueue);
+    append(2, &myQueue);
+    append(3, &myQueue);
+    append(50, &myQueue);
 
     printf("\nQueue Size: %d\n",queueSize(&myQueue));
 
-    for(i=0;i < 3; i++)
-    {
-        serve(&temp,&myQueue);
-        printf("Served %d\n",temp);
+    for(i = 0;i < 3; i++) {
+        serve(&temp, &myQueue);
+        printf("Served %d\n", temp);
     }
 
     peek(&myQueue);
     return 0;
+
 }
