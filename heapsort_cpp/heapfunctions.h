35aac1268673f997167aba1ec7693f79a75bfe6e
muvvasandeep_cs16b017 <35159794+muvvasandeep@users.noreply.github.com>
2018-10-12 16:09:50 +0530

    Add files via upload

---
+++ heapsort_cpp/heapfunctions.h
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+class heap
+{
+	public:
+		void heapify(int arr[], int n, int i);
+		void heapSort(int arr[], int n);
+		void printArray(int arr[], int n);	
+};
+
