ca74b3dd8fb0e13bd3ae8b8eca332cc194d4fc04
SAKSHI-CHANDEL <31941156+SAKSHI-CHANDEL@users.noreply.github.com>
2018-10-21 22:32:21 +0530

    Update MergeSort.cpp

--- merge_sort/Cpp/MergeSort.cpp
+++ merge_sort/Cpp/MergeSort.cpp
@@ -5,38 +5,38 @@
 using namespace std;
 
 void merge(int a[], int p, int q, int r){
-    int b[50],i = p, j = q + 1, k = 0;
-    while(i <= q && j <= r){
-        if(a[i] < a[j])
-        	b[k++] = a[i++];    
-        else
-        	b[k++] = a[j++];
-    }
-    while(i <= q){
-        b[k++] = a[i++];
+	int b[50],i = p, j = q + 1, k = 0;
+	while(i <= q && j <= r){
+		if(a[i] < a[j])
+			b[k++] = a[i++];
+		else
+			b[k++] = a[j++];
 	}
-    while(j <= r){
-    	b[k++] = a[j++];
-    }
-  	for(i = r; i >= p; i--){
-        a[i] = b[--k];  // copying back the sorted list to a[]
+	while(i <= q){
+		b[k++] = a[i++];
+	}
+	while(j <= r){
+		b[k++] = a[j++];
+	}
+	for(i = r; i >= p; i--){
+		a[i] = b[--k];  // copying back the sorted list to a[]
 	}
 }
 
 void mergesort(int a[], int p, int r ){
 	int q;
-    if(p < r){
-    	q = (p + r)/2;
-        mergesort(a,p,q);
-        mergesort(a,q+1,r);
-        merge(a,p,q,r);
-    }
+	if(p < r){
+		q = (p + r)/2;
+		mergesort(a,p,q);
+		mergesort(a,q+1,r);
+		merge(a,p,q,r);
+	}
 }
 
 void display(int a[], int n){
-    int i;
+	int i;
 	for(i = 0 ;i < n; i++)
-        cout<<a[i]<<"\t";
+		cout<<a[i]<<"\t";
 }
 
 int main(){
@@ -45,7 +45,7 @@ int main(){
 	cin>>n;
 	cout<<"\nEnter elements : ";
 	for(i = 0; i < n; i++)
-   		cin>>a[i];
+		cin>>a[i];
 	mergesort(a,0,n-1);
 	cout<<"\nSorted Array :\t";
 	display(a,n);
