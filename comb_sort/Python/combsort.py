2068e3cabcd897f4ee885547193f7b2949386d24
David <32831710+ServinDC@users.noreply.github.com>
2018-10-26 17:07:34 -0500

    Update combsort.py

--- comb_sort/Python/combsort.py
+++ comb_sort/Python/combsort.py
@@ -12,3 +12,12 @@ def combsort(numbers_list):
 				swaps = True
 		gap = max(gap-1, 1)	# update gap, minimum gap is 1
 	return ordered
+
+def main():
+    arr = [12, 96, 24, 48, 36, 60, 84, 72]    # Declaring a sample array
+    combsort(arr)  # Calling the combsort function and passing the array
+    print('Sorted array is: {}'.format(' '.join(map(str, arr))))
+
+
+if __name__ == '__main__':
+    main()
