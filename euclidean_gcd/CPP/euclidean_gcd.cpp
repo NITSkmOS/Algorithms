59b18bce02c902ed927117a85c309460b3b86583
Jeremias Moreira Gomes <j3r3miasmg@gmail.com>
2018-10-30 04:03:41 -0300

    Whitespaces.

--- euclidean_gcd/CPP/euclidean_gcd.cpp
+++ euclidean_gcd/CPP/euclidean_gcd.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-int gcd_rec(int a, int b)
-{
+int gcd_rec(int a, int b) {
 	return b == 0 ? a : gcd_rec(b, a % b);
 }
 int gcd_loop(int a , int b) {
-	while (b != 0)
-	{
+	while (b != 0) {
 		int temp = b;
 		b = a % b;
 		a = temp;
@@ -13,8 +11,7 @@ int gcd_loop(int a , int b) {
 	return a;
 }
 using namespace std;
-int main()
-{
+int main() {
 	int a = 30, b = 40;
 	int x, y;
 
