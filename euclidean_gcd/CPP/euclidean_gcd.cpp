3ce3069ab8840937ea0acf4f19763f0e46203047
Jeremias Moreira Gomes <j3r3miasmg@gmail.com>
2018-10-30 03:51:56 -0300

    Tab correction.

--- euclidean_gcd/CPP/euclidean_gcd.cpp
+++ euclidean_gcd/CPP/euclidean_gcd.cpp
@@ -2,13 +2,13 @@
 
 int gcd_rec(int a, int b)
 {
-    return b == 0 ? a : gcd_rec(b, a % b);
+	return b == 0 ? a : gcd_rec(b, a % b);
 }
 
 
 int gcd_loop(int a , int b) {
 	while (b != 0) 
-    {
+	{
 		int temp = b;
 		b = a % b;
 		a = temp;
@@ -21,10 +21,10 @@ using namespace std;
 int main()
 {
 	int a = 30, b = 40;
-    int x, y;
+	int x, y;
 
 	cout << "Recursive: GCD(" << a << ", " << b << ") = " << gcd_rec(a, b) << endl;
-	cout << "Loop     : GCD(" << a << ", " << b << ") = " << gcd_loop(a, b) << endl;
+	cout << "Loop	 : GCD(" << a << ", " << b << ") = " << gcd_loop(a, b) << endl;
 
-    return 0;
+	return 0;
 }
