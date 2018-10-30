251f283b4bde0276570fcb6a3e567595eec46b5f
Jeremias Moreira Gomes <j3r3miasmg@gmail.com>
2018-10-30 02:58:43 -0300

    binary_search.c Add Binary Search Algorithm
    This add Binary Search Algorithm which return the index of the searched item.
    
    Closes https://github.com/NITSkmOS/Algorithms/issues/53

---
+++ binary_search/C/binary_search.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int start, int end, int item)
+{
+	int mid = start + (end-start) / 2;
+	while (start <= mid) {
+		if (arr[mid] == item) {
+			return mid;
+		} else if (arr[mid] > item) {
+			end = mid-1;
+		} else {
+			start = mid+1;
+		}
+		mid = start + (end-start) / 2;
+	}
+	return -1;
+}
+
+int main()
+{
+	int arr[] = {1, 3, 4, 4, 10, 13, 25, 26, 27, 60};
+	int n = 10;
+	int x = 13;
+	int result = binarySearch(arr, 0, n - 1, x);
+    printf("Index of %d in array is: %d\n", x, result);
+	return 0;
+}
