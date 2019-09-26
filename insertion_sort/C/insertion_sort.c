9b266b00744482e55d962a8c53318821c9920fbb
Shreya Rastogi <43791350+ShreyaR14@users.noreply.github.com>
2019-09-26 19:12:50 +0530

    Update insertion_sort.c

--- insertion_sort/C/insertion_sort.c
+++ insertion_sort/C/insertion_sort.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
-void insertion_sort(int arr_size, int *arr) {
+void insertion_sort(int arr[], int *arr) {
 	// Fuction to do insertion sort.
 	int i;
 	int pivot;
 	int j;
-	for (i = 1; i < arr_size; i++) {
+	for (i = 1; i < *arr; i++) {
 		pivot = arr[i];
 		j = i-1;
 		/* Move elements of arr[0..i-1], that are
@@ -20,13 +20,19 @@ void insertion_sort(int arr_size, int *arr) {
 	}
 }
 
-int main() {
-	int arr_size = 6, i;
-	int arr[6] = {6, 5, 4, 3, 2, 1};
-	insertion_sort(arr_size, arr);
+void printArray(int arr[], int *arr) 
+{ 
+    int i; 
+    for (i = 0; i < *arr; i++) 
+        printf("%d ", arr[i]); 
+    printf("\n"); 
+} 
 
-	for (i=0; i < arr_size; i++) {
-		printf("%d\n", arr[i]);
-	}
+int main() {
+	int arr[6] = {6, 5, 4, 3, 2, 1};
+	int *arr= sizeof(arr)/ sizeof(arr[0]);
+	insertion_sort(arr, *arr);
+	printArray(arr, *arr);
+
 	return 0;
 }
