ee0fb60814809da193a282693fbc6f754f155eec
abhijeetk698 <abhijeetk698@gmail.com>
2019-10-12 18:46:04 +0530

    addded Eucledian GCD [C++]

---
+++ euclidean_gcd/C++/euclidean_gcd.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+//Calculates GCD of two numbers a & b using the division-based Euclidean Algorithm
+
+int gcd_div(int a , int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+
+//Calculates GCD of two numbers a & b using the recursive-based Euclidean Algorithm
+
+int gcd_rec(int a, int b) {
+    if (a == 0){
+        return b;
+    }
+    return gcd_rec(b % a, a);
+}
+
+
+//Calculates GCD of two numbers a & b using the Recursive-based Extended Euclidean Algorithm
+
+int gcd_extended(int a, int b, int *x, int *y) {
+    if (a == 0) {
+        *x = 0;
+        *y = 1;
+        return b;
+    }
+    int x1, y1;
+    int result = gcd_extended(b % a, a, &x1, &y1);
+    *x = y1 - (b / a) * x1;
+    *y = x1;
+    return result;
+}
+
+// Main Method
+int main() {
+    int a = 20, b = 30, x, y;
+    cout<<"Division :GCD("<<a<<", "<<b<<")"<<" = "<<gcd_div(a,b)<<endl;
+    cout<<"Recursive :GCD("<<a<<", "<<b<<")"<<" = "<<gcd_rec(a,b)<<endl;
+    int gcd_ext = gcd_extended(a, b, &x, &y);
+    cout<<"Extended :GCD("<<a<<", "<<b<<")"<<" = "<<gcd_ext<<endl;
+    return 0;
+}
