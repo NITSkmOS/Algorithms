c8047e0aac32a088f55b10fcfd95afce9c929a02
archi-007 <56483460+archi-007@users.noreply.github.com>
2019-10-16 22:35:11 +0530

    insertion_sort.java
    Implementation of insertion sort in java.

---
+++ insertion_sort/Java/insertion_sort.java
@@ -0,0 +1,37 @@
+class Insertionsort { 
+    void sort(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 1; i < n; ++i) { 
+            int key = arr[i]; 
+            int j = i - 1; 
+  
+            while (j >= 0 && arr[j] > key) { 
+                arr[j + 1] = arr[j]; 
+                j = j - 1; 
+            } 
+            arr[j + 1] = key; 
+        } 
+    } 
+  
+    
+    static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 0; i < n; ++i) 
+            System.out.print(arr[i] + " "); 
+  
+        System.out.println(); 
+    } 
+  
+     
+    public static void main(String args[]) 
+    { 
+        int arr[] = { 47, 4, 16, 29, 32}; 
+  
+        Insertionsort ob = new Insertionsort(); 
+        ob.sort(arr); 
+  
+        printArray(arr); 
+    } 
+} 
