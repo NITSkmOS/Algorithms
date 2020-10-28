93e3ed9d53af491d951f3379cccc51094fc85b2e
vj <vedantj@nova.tech>
2020-10-28 21:53:31 +0530

    BubbleSort classes and method renamed

--- bubble_sort/Java/BubbleSort.java
+++ bubble_sort/Java/BubbleSort.java
@@ -1,15 +1,15 @@
-public class bubble_sort {
+public class BubbleSort {
     public static void main(String[] args){
     //Taking a sample array for sorting it.
         int[] nums = {12, 45, 23, 78, 67};
 
     //calling the method and passing the array in it.
-        bubble(nums, nums.length);
+        bubbleSort(nums, nums.length);
     //Printing the sorted array
         print_sorted_array(nums);
     }
     //The recurisve method that takes in the array and the length of it.
-    public static void bubble(int[] nums, int end){
+    public static void bubbleSort(int[] nums, int end){
     //base case
         if (end == 1) {
             return;
@@ -23,7 +23,7 @@ public class bubble_sort {
             }
         }
     //calling the method again to sort the rest of the elements on next iteration
-        bubble(nums, end - 1);
+        bubbleSort(nums, end - 1);
     }
 
     //Method to print an array
